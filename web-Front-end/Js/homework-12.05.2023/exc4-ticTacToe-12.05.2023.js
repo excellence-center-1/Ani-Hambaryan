6abebac74d753c2e02a8c3bf503bc8e6f2c9e383
Instigate Mobile | Naira Poghosyan <naira>
2023-05-16 15:35:20 +0400

    Did minor change.

--- web-Front-end/Js/homework-12.05.2023/exc4-ticTacToe-12.05.2023.js
+++ web-Front-end/Js/homework-12.05.2023/exc4-ticTacToe-12.05.2023.js
@@ -2,40 +2,44 @@
 let count = 1;
 let board = []
 const size = parseInt(prompt("Enter the size of the board - 3 or 4: "))
-let print_board = () => {};
+const BOARD_SIZE = 4;
+let print_board;
 
-if(size === 4){
+
+if (size === BOARD_SIZE) {
   board = [
-  [" ", " ", " ", " "],
-  [" ", " ", " ", " "],
-  [" ", " ", " ", " "],
-  [" ", " ", " ", " "]
-];
-print_board = () => {
-  console.log(`
+    [" ", " ", " ", " "],
+    [" ", " ", " ", " "],
+    [" ", " ", " ", " "],
+    [" ", " ", " ", " "]
+  ];
+
+  print_board = () => {
+    console.log(`
         1       2        3        4
     ._______.________.________.________.
-    |       |        |        |        |             
+    |       |        |        |        |
   1 |   ${board[0][0]}   |   ${board[0][1]}    |   ${board[0][2]}    |   ${board[0][3]}    |
     |_______|________|________|________|
-    |       |        |        |        |             
+    |       |        |        |        |
   2 |   ${board[1][0]}   |   ${board[1][1]}    |   ${board[1][2]}    |   ${board[1][3]}    |
     |_______|________|________|________|
-    |       |        |        |        |             
+    |       |        |        |        |
   3 |   ${board[2][0]}   |   ${board[2][1]}    |   ${board[2][2]}    |   ${board[2][3]}    |
     |_______|________|________|________|
-    |       |        |        |        |             
+    |       |        |        |        |
   4 |   ${board[3][0]}   |   ${board[3][1]}    |   ${board[3][2]}    |   ${board[3][3]}    |
     |_______|________|________|________|  `);
-} } else {
+  }
+} else {
   board = [
-  [" ", " ", " "],
-  [" ", " ", " "],
-  [" ", " ", " "],
-];
-print_board = () => {
-  console.log(`
-          1       2        3     
+    [" ", " ", " "],
+    [" ", " ", " "],
+    [" ", " ", " "],
+  ];
+  print_board = () => {
+    console.log(`
+          1       2        3
     ._______.________.________.
     |       |        |        |
   1 |   ${board[0][0]}   |   ${board[0][1]}    |   ${board[0][2]}    |
@@ -43,29 +47,25 @@ print_board = () => {
     |       |        |        |
   2 |   ${board[1][0]}   |   ${board[1][1]}    |   ${board[1][2]}    |
     |_______|________|________|
-    |       |        |        | 
+    |       |        |        |
   3 |   ${board[2][0]}   |   ${board[2][1]}    |   ${board[2][2]}    |
     |_______|________|________|`);
-};
+  };
 };
 
 
-let choose_player = (count) => {
-  if (count % 2 == 0) {
-    return "O";
-  }
-  return "X";
-}
+const choose_player = (count) => count % 2 === 0 ? "O" : "X";
 
 let input_value = (player) => {
   console.log(`${player}\'s tourn.`);
-  let choice_row, choice_col;
-  choice_row = parseInt(prompt(`Choose a row from 1-${size}  - `)) - 1;
-  choice_col = parseInt(prompt(`Choose a column from 1-${size}  - `)) - 1;
-  if (isNaN(choice_row) || choice_row < 0 || choice_row > size-1 || isNaN(choice_col) || choice_col < 0 || choice_col > size-1) {
+  const choice_row = parseInt(prompt(`Choose a row from 1-${size}  - `)) - 1;
+  const choice_col = parseInt(prompt(`Choose a column from 1-${size}  - `)) - 1;
+  const isCorrectRowSize = isNaN(choice_row) || choice_row < 0 || choice_row > size - 1;
+  const isCorrectColSize = isNaN(choice_col) || choice_col < 0 || choice_col > size - 1;
+  if (isCorrectRowSize || isCorrectColSize) {
     console.log(`Invalid input! Choose a correct position from 1-${size}`);
     input_value(player);
-  } else if (board[choice_row][choice_col] != " ") {
+  } else if (board[choice_row][choice_col] !== " ") {
     console.log("Invalid input! Position already taken.");
     input_value(player);
   } else {
@@ -104,7 +104,7 @@ const isWinner = (symbol, board) => {
   return false;
 }
 
-let game = () => {
+const game = () => {
   console.log("First player - X     second player - O\n-------------------------------------");
   print_board();
   while (true) {
@@ -112,7 +112,7 @@ let game = () => {
     if (isWinner(choose_player(count), board)) {
       console.log(`Congratulations! ${choose_player(count)}\`s has won`);
       break;
-    } else if (count == size * size) {
+    } else if (count === size ^ 2) {
       console.log("The game end");
       break;
     }
