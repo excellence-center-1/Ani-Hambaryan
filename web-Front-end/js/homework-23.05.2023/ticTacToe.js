8624f041fcdf729f055070aa82a8a72056d59621
Ani Hambaryan <ani.hambaryan.ec@gmail.com>
2023-05-26 23:06:06 +0400

    a

--- web-Front-end/js/homework-23.05.2023/ticTacToe.js
+++ web-Front-end/js/homework-23.05.2023/ticTacToe.js
@@ -1,7 +1,12 @@
 let count = 1;
 let boardSize = 0;
 let board;
-const choose_player = (count) => (count % 2 === 0 ? "O" : "X");
+
+
+const choose_player = (() => {
+  let count = 1;
+  return () => (count++ % 2 === 0 ? "O" : "X");
+})();
 
 const printBoard = () => {
   const boardElement = document.getElementById("board");
@@ -11,14 +16,16 @@ const printBoard = () => {
       const cell = document.createElement("button");
       cell.className = "cell";
       cell.textContent = board[i][j];
-      cell.draggable = false;
       cell.addEventListener("click", () => {
         if (cell.textContent === '') {
           cell.textContent = choose_player(count);
+          count++;
+        } else {
+          printBoard();
         }
-        updateBoard(i, j, button_cell.textContent);
+        updateBoard(i, j, cell.textContent);
       });
-      boardElement.appendChild(button_cell);
+      boardElement.appendChild(cell);
     }
     boardElement.appendChild(document.createElement("br"));
   }
@@ -26,13 +33,12 @@ const printBoard = () => {
 
 const game = () => {
   boardSize = parseInt(document.getElementById('sizeb').value);
-board = new Array(boardSize).fill().map(() => new Array(boardSize).fill());
+  board = new Array(boardSize).fill().map(() => new Array(boardSize).fill());
   printBoard();
 };
 
 const updateBoard = (row, col, value) => {
   board[row][col] = value;
-  count++;
   if (isWinner(value, board)) {
     document.getElementById("message").textContent = `Congratulations! ${value} has won.`;
   } else if (count === boardSize ** 2) {
