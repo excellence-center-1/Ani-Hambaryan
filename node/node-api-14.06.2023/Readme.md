c6524c5f27ca70070f38d5b3f68608662dac6fcd
Ani Hambaryan <ani.hambaryan.ec@gmail.com>
2023-06-20 00:23:52 +0400

    swagger

--- node/node-api-14.06.2023/Readme.md
+++ node/node-api-14.06.2023/Readme.md
@@ -1,15 +1,22 @@
+npm install swagger-ui-express
+
 
 **Post API**
-*curl -X POST http://localhost:3000/add*
+
+*curl -X POST -H "Content-Type: application/json" -d '{"id": 5, "name":"your_username","actualName":"null", "displayName":"AA"}' http://localhost:3000/add*
+
 
 **Delete API**
+
 *curl -X DELETE http://localhost:3000/users/:id*
 
+
 **Put API**
+
 *curl -X PUT -d '{"name":"???",  "actualName": "???"}' -H "Content-Type: application/json" http://localhost:3000/users/:id*
 
-**login.js**
-*curl -X POST -H "Content-Type: application/json" -d '{"username":"your_username","password":"your_password"}' http://localhost:3000/login
 
-*
+**login.js**
+
+*curl -X POST -H "Content-Type: application/json" -d '{"username":"your_username","password":"your_password"}' http://localhost:3000/login*
 
--- node/node-api-14.06.2023/app.js
+++ node/node-api-14.06.2023/app.js
@@ -1,129 +1,136 @@
-
-
 const express = require('express');
-const fs = require('fs')
+const fs = require('fs');
 const bodyParser = require('body-parser');
+const swaggerUi = require('swagger-ui-express');
+const swaggerDocument = require('./swagger.json');
 const port = 3000;
 const app = express();
 app.use(bodyParser.json());
+
 app.get('/', (req, res) => {
   res.send('Hello World');
 });
 
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+
 app.get('/users', (req, res) => {
   fs.readFile('users.json', (err, data) => {
     if (err) {
-      console.error(err)
-      return
+      console.error(err);
+      res.status(500).send('Internal Server Error');
+      return;
     }
     console.log(data);
     res.end(data);
-  })
-})
+  });
+});
 
 app.get('/users/:id', (req, res) => {
   const userId = req.params.id;
   fs.readFile('users.json', (err, data) => {
     if (err) {
-      console.error(err)
-      return
+      console.error(err);
+      res.status(500).send('Internal Server Error');
+      return;
     }
     const users = JSON.parse(data);
-    const user = users.find(user => user.id === userId);
-    user ? res.json(user) : res.sendStatus(404).send('User not found')
+    const user = users.find((user) => user.id === userId);
+    if (!user) {
+      res.status(404).send('User not found');
+      return;
+    }
+    res.json(user);
   });
 });
 
 app.post('/add', (req, res) => {
-  const newUser = {
-
-    "id": "5",
-    "name": "admin5",
-    "actualName": null,
-    "displayName": "admin5"
-
-  };
-
+  const newUser = req.body;
+  if (!newUser || Object.keys(newUser).length === 0) {
+    res.status(400).send('Bad Request: Invalid user data');
+    return;
+  }
   fs.readFile('users.json', (err, data) => {
     if (err) {
-      console.error(err)
-      return
+      console.error(err);
+      res.status(500).send('Internal Server Error');
+      return;
     }
     const users = JSON.parse(data);
     users.push(newUser);
-    fs.writeFile('users.json', JSON.stringify(users, null, 2), err => {
+    fs.writeFile('users.json', JSON.stringify(users, null, 2), (err) => {
       if (err) {
-        console.error(err)
-        return
+        console.error(err);
+        res.status(500).send('Internal Server Error');
+        return;
       }
       res.json(newUser);
-    })
-  })
-})
+    });
+  });
+});
 
 app.delete('/users/:id', (req, res) => {
   const userId = req.params.id;
   fs.readFile('users.json', (err, data) => {
     if (err) {
-      console.error(err)
-      return
+      console.error(err);
+      res.status(500).send('Internal Server Error');
+      return;
     }
     const users = JSON.parse(data);
-    const userIndex = users.findIndex(user => user.id === userId);
-    if (!userIndex) {
-      res.sendStatus(404).send('User not found');
+    const userIndex = users.findIndex((user) => user.id === userId);
+    if (userIndex === -1) {
+      res.status(404).send('User not found');
+      return;
     }
     const removedUser = users.splice(userIndex, 1);
-    fs.writeFile('users.json', JSON.stringify(users, null, 2), err => {
+    fs.writeFile('users.json', JSON.stringify(users, null, 2), (err) => {
       if (err) {
-        console.error(err)
-        return
+        console.error(err);
+        res.status(500).send('Internal Server Error');
+        return;
       }
       res.json(removedUser[0]);
-    })
-  })
+    });
+  });
 });
 
 const middlePut = (req, res, next) => {
-    console.log(`PUT request received for path: ${req.path}`)
-    console.log('bodi', req.body);
-    next();
-  }
+  console.log(`PUT request received for path: ${req.path}`);
+  console.log('bodi', req.body);
+  next();
+};
 
-  
 app.put('/users/:id', middlePut, (req, res) => {
-    const userId = req.params.id;
-    const updatedUserData = req.body;
-  
-    fs.readFile('users.json', (err, data) => {
+  const userId = req.params.id;
+  const updatedUserData = req.body;
+
+  fs.readFile('users.json', (err, data) => {
+    if (err) {
+      console.error(err);
+      res.status(500).send('Internal Server Error');
+      return;
+    }
+    const users = JSON.parse(data);
+    const userIndex = users.findIndex((user) => user.id === userId);
+    if (userIndex === -1) {
+      res.status(404).send('User not found');
+      return;
+    }
+    users[userIndex] = {
+      ...users[userIndex],
+      ...updatedUserData,
+    };
+    fs.writeFile('users.json', JSON.stringify(users, null, 2), (err) => {
       if (err) {
         console.error(err);
+        res.status(500).send('Internal Server Error');
         return;
       }
-      const users = JSON.parse(data);
-      const userIndex = users.findIndex((user) => user.id === userId);
-      if (userIndex === -1) {
-        res.status(404).send('User not found');
-        return;
-      }
-      users[userIndex] = {
-        ...users[userIndex],
-        ...updatedUserData,
-      };
-      fs.writeFile('users.json', JSON.stringify(users, null, 2), (err) => {
-        if (err) {
-          console.error(err);
-          return;
-        }
-        res.json(users[userIndex]);
-      });
+      res.json(users[userIndex]);
     });
   });
+});
 
-
-
-
-  app.listen(port, () => {
-    console.log(`API server listening on port ${port}`);
-  });
-  
+app.listen(port, () => {
+  console.log(`API server listening on port ${port}`);
+});
