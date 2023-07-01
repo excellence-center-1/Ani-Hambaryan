cd71a01d0422b7a13aed0a134a2a81bcb5d4bb86
Ani Hambaryan <ani.hambaryan.ec@gmail.com>
2023-07-02 01:16:01 +0400

    GuessWord

--- node/guess_word_sequelize/client/src/Login.js
+++ node/guess_word_sequelize/client/src/Login.js
@@ -5,117 +5,95 @@ import { Link, useNavigate } from 'react-router-dom';
 
 
 export const Login = () => {
-    const PWD_REGEX = /^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.{8,})/;
-    const USER_REGEX = /^[A-Z][a-zA-Z0-9_-]{2,15}$/;
-    const navigate = useNavigate();
-    const errRef = useRef();
-    const [user, setUser] = useState('');
-    const [pwd, setPwd] = useState('');
-    const [errMsg, setErrMsg] = useState('');
-    const [success, setSuccess] = useState(false);
+  const PWD_REGEX = /^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.{8,})/;
+  const USER_REGEX = /^[A-Z][a-zA-Z0-9_-]{2,15}$/;
+  const navigate = useNavigate();
+  const errRef = useRef();
+  const [user, setUser] = useState('');
+  const [pwd, setPwd] = useState('');
+  const [errMsg, setErrMsg] = useState('');
+  const [success, setSuccess] = useState(false);
+  const [token, setToken] = useState('');
+  useEffect(() => {
+    setErrMsg('');
+  }, [user, pwd])
 
-    useEffect(() => {
-        setErrMsg('');
-    }, [user, pwd])
 
-
-    const handleInputChange = (e, fieldName) => {
-        const value = e.target.value;
-        if (fieldName === 'user') {
-            setUser(value)
-        } else if (fieldName === "password") {
-            setPwd(value)
-        }
+  const handleInputChange = (e, fieldName) => {
+    const value = e.target.value;
+    if (fieldName === 'user') {
+      setUser(value)
+    } else if (fieldName === "password") {
+      setPwd(value)
     }
+  }
 
-    const createUserLevel = async (userId, score) => {
-        try{
-const response = await fetch('http://localhost:4000/getLevel', {
-    method: 'POST',
-    headers: {
-        'Content-Type': 'application/json',
-    },
-    body: JSON.stringify({ score }),
-});
-if (response.ok) {
-    const {levelId} = await response.json();
-    await fetch('http://localhost:4000/score', {
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const validUs = USER_REGEX.test(user);
+    const validPass = PWD_REGEX.test(pwd);
+    if (!validUs || !validPass) {
+      setErrMsg('Invalid Name or password');
+      return;
+    }
+    try {
+      // const hash = bcrypt.hashSync(pwd, 10)
+      const response = await fetch('http://localhost:4000/login', {
         method: 'POST',
         headers: {
-            'Content-Type': 'application/json',
+          'Content-Type': 'application/json',
         },
-        body: JSON.stringify({ userId, levelId, score }),
-    });
-} else {
-    console.log('Error retrieving level');
-}
-} catch (error) {
-console.error('Error creating user_level', error);
-}
-};
+        body: JSON.stringify({ username: user, password: pwd }),
+      });
+      const data = await response.json();
+      if (response.ok) {
+        const token = data.token; //
 
-    const handleSubmit = async (e) => {
-        e.preventDefault();
-        const validUs = USER_REGEX.test(user);
-        const validPass = PWD_REGEX.test(pwd);
-        if (!validUs || !validPass) {
-            setErrMsg('Invalid Name or password');
-            return;
-        }
-        try {
-            // const hash = bcrypt.hashSync(pwd, 10)
-            const response = await fetch('http://localhost:4000/login', {
-                method: 'POST',
-                headers: {
-                    'Content-Type': 'application/json',
-                },
-                body: JSON.stringify({ username: user, password: pwd}),
-            });
-            const data = await response.json();
-            if (response.ok) {
-                const token = data.token; //
-                // localStorage.setItem('token', token);
-                document.cookie = `token=${token};`; //
-
-                setSuccess(true);
-                setErrMsg('Login Success');
-                console.log('User logged in:', { username: user });
-                // const { userId, score } = data; 
-                // createUserLevel(userId, score); 
-                setTimeout(() => {
-                    navigate('/game');
-                }, 1000);
-            } else {
-                setSuccess(false);
-                setErrMsg('invalid username or password');
-            }
-        } catch (err) {
-            setSuccess(false);
-            setErrMsg('invalid username or password');
-        }
-    };
+        // localStorage.setItem('token', token);
+        document.cookie = `token=${token};`; //
+        setToken(token);
 
 
-    return (
-        <div className="container w-25 mt-5 bg-danger-subtle p-4">
-            <p ref={errRef} className={errMsg ? "errmsg" : "offscreen"} >{errMsg}</p>
-            <h1>Login</h1>
-            <form className="d-flex flex-column p-5 mt-5">
-                <div className="mb-3">
-                    <label className="form-label">User Name</label>
-                    <input type="text" name = 'user' value={user} onChange={(e) => handleInputChange(e, 'user')} className="form-control" />
-                </div>
-                <div className="mb-3">
-                    <label className="form-label">Password</label>
-                    <input type="password" name = "password"value={pwd} onChange={(e) => handleInputChange(e, 'password')} className="form-control" />
+        setSuccess(true);
+        setErrMsg('Login Success');
+        console.log('User logged in:', { username: user });
+        // const { userId, score } = data; 
+        // createUserLevel(userId, score); 
+        setTimeout(() => {
+          navigate('/game');
+        }, 1000);
+      } else {
+        setSuccess(false);
+        setErrMsg('invalid username or password');
+      }
+    } catch (err) {
+      setSuccess(false);
+      setErrMsg('invalid username or password');
+    }
+  };
 
-                </div>
-                <div className="mb-3">
-                    {/* <Link to = "/game" onClick={handleSubmit} className="btn btn-primary">Login</Link> */}
-                    <button type="submit" onClick={handleSubmit} className="btn btn-secondary">Login</button>
-                </div>
-            </form>
-            <Link to="/" className="text-secondary">Signup</Link>
+
+  return (
+    <div className="container w-25 mt-5 bg-danger-subtle p-4">
+      <p ref={errRef} className={errMsg ? "errmsg" : "offscreen"} >{errMsg}</p>
+      <h1>Login</h1>
+      <form className="d-flex flex-column p-5 mt-5">
+        <div className="mb-3">
+          <label className="form-label">User Name</label>
+          <input type="text" name='user' value={user} onChange={(e) => handleInputChange(e, 'user')} className="form-control" />
         </div>
-    )
+        <div className="mb-3">
+          <label className="form-label">Password</label>
+          <input type="password" name="password" value={pwd} onChange={(e) => handleInputChange(e, 'password')} className="form-control" />
+
+        </div>
+        <div className="mb-3">
+          {/* <Link to = "/game" onClick={handleSubmit} className="btn btn-primary">Login</Link> */}
+          <button type="submit" onClick={handleSubmit} className="btn btn-secondary">Login</button>
+        </div>
+      </form>
+      <Link to="/" className="text-secondary">Signup</Link>
+    </div>
+  )
 }
--- node/guess_word_sequelize/client/src/guessWord.js
+++ node/guess_word_sequelize/client/src/guessWord.js
@@ -1,127 +1,147 @@
 import React, { useEffect, useState } from "react";
 import 'bootstrap/dist/css/bootstrap.css';
 import { useNavigate } from 'react-router-dom';
+import { useLocation } from 'react-router-dom';
 export const GuessWord = () => {
-    const inputs = [];
-    const [guess, setGuess] = useState('');
-    const [message, setMessage] = useState('');
-    const [count, setCount] = useState(1);
-    const [currentWord, setCurrentWord] = useState('');
-    const [currentquestion, setCurrentQuestion] = useState('')
-    const [user_id, setUser_id] = useState(null); 
-    const [level_id, setLevel_id] = useState(null);
-    const navigate = useNavigate();
+  const location = useLocation();
+  const token = location.state?.token || '';
+  const inputs = [];
+  const [guess, setGuess] = useState('');
+  const [message, setMessage] = useState('');
+  const [message1, setMessage1] = useState('');
+  const [count, setCount] = useState(1);
+  const [currentWord, setCurrentWord] = useState('');
+  const [currentQuestion, setCurrentQuestion] = useState('')
+  const [levelId, setLevelId] = useState(1);
+  const [prevQuestionIds, setPrevQuestionIds] = useState([]);
+  const [currentQuestionId, setCurrentQuestionId] = useState(null);
+  const navigate = useNavigate();
 
-useEffect(() => {
-    const fetchUserData = async () => {
-        try {
-            const response = await fetch("http://localhost:4000/userid");
-            if (response.ok) {
-              const data = await response.json();
-              setUser_id(data.user_id);
-              setLevel_id(data.level_id);
-            } else {
-              console.log("Error retrieving user data");
-            }
-          } catch (error) {
-            console.error("Error retrieving user data", error);
-          }
-        };
-    
-        fetchUserData();
-      }, []);
-
-      
-    const fetchRandomWord = async () => {
-        try {
-            const response = await fetch('http://localhost:4000/random-word', {
-                method: 'GET',
-            });
-            if (response.ok) {
-                const data = await response.json();
-
-                setCurrentWord(data.question.word);
-                setCurrentQuestion(data.question.question)
-            } else {
-                console.error('Error retrieving random word', response.status);
-            }
-        } catch (error) {
-            console.error('Error retrieving random word', error);
-        }
-    };
-
-
-    useEffect(() => {
-        fetchRandomWord();
-      }, []);
-
-    const handleInput = (e) => {
-        const index = parseInt(e.target.name); //տալիս է զանգվածի սիմվոլին արժեք
-        const character = e.target.value;
-
-        setGuess((prevGuess) => {
-            const updatedGuess = [...prevGuess]; //հին զանգվաշը կրկնօրինակում է 
-            updatedGuess[index] = character;
-            return updatedGuess;
-        });
-    };
+  useEffect(() => {
+    if (count >= 5) {
+      setLevelId(2);
+      setMessage1('Level 2');
+    } else if (count >= 8) {
+      setLevelId(2);
+      setMessage1('Level 3');
+    } else {
+      setMessage1('Level 1');
+    }
+  }, [count]);
+  const fetchRandomWord = async () => {
+    try {
+      const response = await fetch(`http://localhost:4000/random-word?level_id=${levelId}`, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({ exclude: prevQuestionIds })
+      });
+      if (response.ok) {
+        const data = await response.json();
+        setCurrentWord(data.question.word);
+        setCurrentQuestion(data.question.question)
+        setCurrentQuestionId(data.question.id);
+        // navigate('/game', { state: { token } });
+      } else {
+        console.error('Error retrieving random word', response.status);
+      }
+    } catch (error) {
+      console.error('Error retrieving random word', error);
+    }
+  };
 
 
-    for (let i = 0; i < currentWord?.length; i++) {
-        inputs.push(<input key={i} name={i.toString()} type="text" value={guess[i] || ''} onChange={(e) => handleInput(e)} className='input-field me-2 rounded-2 bg-info bg-opacity-10 border border-info inp' />);
+  useEffect(() => {
+    fetchRandomWord();
 
+    setTimeout(() => {
+      navigate('/game', { state: { token } });
+    }, 1000);
+  }, [levelId]);
+
+  const handleInput = (e) => {
+    const index = parseInt(e.target.name); //տալիս է զանգվածի սիմվոլին արժեք
+    const character = e.target.value;
+
+    setGuess((prevGuess) => {
+      const updatedGuess = [...prevGuess]; //հին զանգվաշը կրկնօրինակում է 
+      updatedGuess[index] = character;
+      return updatedGuess;
+    });
+  };
+
+
+  for (let i = 0; i < currentWord?.length; i++) {
+    inputs.push(<input key={i} name={i.toString()} type="text" value={guess[i] || ''} onChange={(e) => handleInput(e)} className='input-field me-2 rounded-2 bg-info bg-opacity-10 border border-info inp' />);
+
+  }
+
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const guessWord = guess.join('');
+    if (guessWord === currentWord) {
+      setMessage('You guessed the word');
+      setCount((prevCount) => prevCount + 1);
+
+    } else {
+      setMessage('You didn\'t guess the word');
+      setCount((count > 1) ? (prevCount) => prevCount - 1 : count);
+    }
+    setGuess('');
+    setPrevQuestionIds((prevIds) => [...prevIds, currentQuestionId]);
+    if (count === 5) {
+      await fetch('http://localhost:4000/users_levels', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({ level_id: 2, score: count })
+      });
+    } else if (count === 8) {
+      await fetch('http://localhost:4000/users_levels', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({ level_id: 3, score: count })
+      });
     }
 
-    
-      const handleSubmit = async (e) => {
-        e.preventDefault();
-        const guessWord = guess.join('');
-        if (guessWord === currentWord) {
-          setMessage('You guessed the word');
-          setCount((prevCount) => prevCount + 1);
-          
-        } else {
-          setMessage('You didn\'t guess the word');
-          setCount((count > 1) ? (prevCount) => prevCount - 1 : count);
-        }
-        setGuess('');
-        if (count < 5) {
-            setLevel_id(1); // Level 1 - Beginner
-          } else if (count < 8) {
-            setLevel_id(2); // Level 2 - Mid1
-          } else {
-            setLevel_id(3); // Level 3 - Mid2
-          }
     fetchRandomWord();
-     
-      };
 
-    const handleSubmitEnd = (e) => {
-        e.preventDefault();
-        navigate('/login')
-    };
+  };
 
-    return (
-        <div className="container mt-5">
-            <h1 className='mt-5 p-5'>================= <span className="text-info">Word guessing game</span> =================</h1>
-            <h4><span className="text-info">Question:</span> {currentquestion}</h4>
-            <div className='d-flex justify-content-center container mt-5'>
+  const handleSubmitEnd = (e) => {
+    e.preventDefault();
+    navigate('/login')
+    return;
+  };
 
-                {inputs}
+  return (
+    <div className="container mt-5">
+      <h1 className='mt-5 p-5'>================= <span className="text-info">Word guessing game</span> =================</h1>
+      <h4><span className="text-info">Question:</span> {currentQuestion}</h4>
+      <div className='d-flex justify-content-center container mt-5'>
 
-            </div>
-            <div>
-            <button className="mt-3 btn btn-info" type="submit" onClick={handleSubmit}>Submit</button>
-            <input value={`score: ${count}`} readOnly className="mt-3 btn btn-info ms-2 count" />
-            <h5>{message}</h5>
-            <button className="mt-3 btn btn-info" type="submit" onClick={handleSubmitEnd}>LogOut</button>
-            </div>
-            <h4 className="mt-5 mb-2 ">Subscription</h4>
-            <input type="radio" className="btn-check m-2" id="btncheck1" autocomplete="off" name="choose"/>
-  <label className="btn btn-outline-info m-2" for="btncheck1">Open one letter</label>
-            <input type="radio" className="btn-check m-2" id="btncheck2" autocomplete="off" name="choose"/>
-  <label className="btn btn-outline-info m-2" for="btncheck2">Open two letter</label>
-        </div>
-    );
+        {inputs}
+
+      </div>
+      <div>
+        <button className="mt-3 btn btn-info" type="submit" onClick={handleSubmit}>Submit</button>
+        <input value={`score: ${count}`} readOnly className="mt-3 btn btn-info ms-2 count" />
+        <h5>{message}</h5>
+        <h5>{message1}</h5>
+        <button className="mt-3 btn btn-info" type="submit" onClick={handleSubmitEnd}>LogOut</button>
+
+      </div>
+      <h4 className="mt-5 mb-2 ">Subscription</h4>
+      <input type="radio" className="btn-check m-2" id="btncheck1" autocomplete="off" name="choose" />
+      <label className="btn btn-outline-info m-2" for="btncheck1">Open one letter</label>
+      <input type="radio" className="btn-check m-2" id="btncheck2" autocomplete="off" name="choose" />
+      <label className="btn btn-outline-info m-2" for="btncheck2">Open two letter</label>
+    </div>
+  );
 }
 
