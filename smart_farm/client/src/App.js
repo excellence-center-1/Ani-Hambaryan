4dc0fe3dc575816775d538ed7b4e6652fc44a073
Ani Hambaryan <ani.hambaryan.ec@gmail.com>
2023-08-21 03:37:39 +0400

    a

--- smart_farm/client/src/App.js
+++ smart_farm/client/src/App.js
@@ -1,25 +1,25 @@
-import logo from './logo.svg';
+//client/App.js
 import './App.css';
+import { Admin, Resource } from 'react-admin';
+import restProvider from 'ra-data-simple-rest';
+import { MyLayout } from './layouts/Layout';
+import { CssBaseline } from '@mui/material';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import { MainDashboard } from './pages/MainDashboard';
+import {Signup} from './pages/Signup';
+
+
+const DATAPROVIDER = restProvider('http://localhost:3000');
+
+const  App = ()  => {
+    return (
+        
+        <Admin layout={MyLayout} dataProvider={DATAPROVIDER} >
+            <CssBaseline />
+       <Resource name='DashBoard' list={MainDashboard} />
+       <Resource name='Signup' list={Signup} />
+        </Admin>
+      );
 }
 
 export default App;
